7f8d51691c4e2419266a8ba46f4828a035bc2715
kagchi <yonuxbot@gmail.com>
2023-05-17 10:23:18 +0700

    feat: initial commit

---
+++ packages/fastify-plugin/src/register.ts
@@ -0,0 +1,23 @@
+import { FrameworkClient, Plugin, postInitialization, ClientOptions, preLogin } from "@nezuchan/framework";
+import fastify from "fastify";
+import { container } from "@sapphire/pieces";
+import middie from "@fastify/middie";
+import { RouteStore } from "./Stores/RouteStore.js";
+import { PreHandlerStore } from "./Stores/PreHandlerStore.js";
+
+export class Api extends Plugin {
+    public static [postInitialization](this: FrameworkClient, options: ClientOptions): void {
+        this.server = fastify(options.api);
+        this.stores.register(new RouteStore());
+        this.stores.register(new PreHandlerStore());
+        container.server = this.server;
+    }
+
+    public static async [preLogin](this: FrameworkClient): Promise<void> {
+        await this.server.register(middie);
+        await this.server.listen({ port: this.options.api?.port ?? 3000, host: this.options.api?.host ?? "localhost" });
+    }
+}
+
+FrameworkClient.plugins.registerPostInitializationHook(Api[postInitialization], "API-PostInitialization");
+FrameworkClient.plugins.registerPreLoginHook(Api[preLogin], "API-PreLogin");
