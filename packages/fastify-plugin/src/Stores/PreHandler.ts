5d629ba90450de3b9b5b86895a8fca18b910e2c7
kagchi <yonuxbot@gmail.com>
2023-05-17 12:37:15 +0700

    fix: error types

--- packages/fastify-plugin/src/Stores/PreHandler.ts
+++ packages/fastify-plugin/src/Stores/PreHandler.ts
@@ -15,8 +15,8 @@ export abstract class PreHandler extends Piece {
         this.position = options.position ?? null;
     }
 
-    public error(options: { message: string; code: number }): Awaitable<Result<unknown, ApiError>> {
-        return Result.err(new ApiError(options.message, options.code));
+    public error(options: { message: string; statusCode: number }): Awaitable<Result<unknown, ApiError>> {
+        return Result.err(new ApiError(options.message, options.statusCode));
     }
 
     public ok(): Awaitable<Result<unknown, ApiError>> {
